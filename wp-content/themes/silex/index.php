776fe85e22cdaf385bd035aee389d0a3536d30c7
Nate Garbacz <bazzic@gmail.com>
2011-04-15 15:27:39 +0100

    Added comments to blog posts.

--- wp-content/themes/silex/index.php
+++ wp-content/themes/silex/index.php
@@ -16,11 +16,18 @@
 						<h2><?php the_title(); ?></h2>
 					<?php endif; ?>
 					<?php the_content(); ?>
+					<?php if(is_singular('post')): ?>
+					  <div class="postmetadata"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <span class="tiny-category"><?php the_category(', ') ?></span></span></div>
+            </div>
+            <?php comments_template(); ?>
+          <?php endif; ?>
 				</div>
 			<?php endwhile; ?>
 			</div>
 			
 			<?php get_sidebar();?>
+			
 		</div>
+		
 <?php endif; ?>
 <?php get_footer(); ?>
