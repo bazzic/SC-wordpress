17b256338d251f4ddd800abbecc0fc0098e58298
tomg <tomg@silex-consulting.com>
2011-05-04 19:26:07 +0100

    Posts in category widget

--- wp-content/plugins/silex-widgets/silex-widgets.php
+++ wp-content/plugins/silex-widgets/silex-widgets.php
@@ -15,8 +15,78 @@ function widget1_render() {
 <?php
 }
 
-function silexWidget1_init() {
-  wp_register_sidebar_widget('sw1', 'Silex Widget 1', 'widget1_render');
+class Category_Widget extends WP_Widget {
+  function Category_Widget() {
+    $widget_ops = array( 'classname' => 'blog-category', 'description' => __('List blog posts within the selected category.', 'category') );
+  
+    $control_ops = array('id_base' => 'category-widget' );
+  
+    $this->WP_Widget( 'category-widget', __('Posts in Category', 'category'), $widget_ops, $control_ops );
+  }
+  
+  function widget( $args, $instance ) {
+    extract( $args );
+    $title = apply_filters('widget_title', $instance['title'] );
+    $category = $instance['category'];
+    
+    echo "<li class='widget cat_view_widget'>";
+    
+    if ( $title )
+      echo "<h2>$title</h2>";
+      
+    echo "<ul>";
+    
+    global $post;
+    $args = array('category' => $category );
+    $posts = get_posts($args);  
+      
+    foreach( $posts as $post ) {
+      setup_postdata($post); ?>
+	<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+    <?php }
+    echo '</ul></li>';      
+      
+      
+  }
+    
+  function update( $new_instance, $old_instance ) {
+    $instance = $old_instance;
+    
+    /* Strip tags for title and name to remove HTML (important for text inputs). */
+    $instance['title'] = strip_tags( $new_instance['title'] );
+    $instance['category'] = strip_tags( $new_instance['category'] );
+    
+    return $instance;
+  }    
+
+
+  function form( $instance ) {
+  
+    /* Set up some default widget settings. */
+    $defaults = array( 'title' => __('Posts in Category', 'category-widget'));
+    $instance = wp_parse_args( (array) $instance, $defaults ); ?>
+    
+    <!-- Widget Title: Text Input -->
+    <p>
+      <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title:', 'hybrid'); ?></label>
+      <input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" />
+    </p>
+    
+    <p>
+    <?php wp_dropdown_categories(array('hide_empty' => 0, 'name' => $this->get_field_name( 'category' ), 'id' => $this->get_field_id( 'category' ), 'hierarchical' => true, 'selected' => $instance['category'])); ?>
+    </p>
+    <?php
+  }    
+  
 }
 
-add_action("plugins_loaded", "silexWidget1_init");
+function silexWidget_init() {
+  wp_register_sidebar_widget('sw1', 'Silex Widget 1', 'widget1_render');	// image splash
+}
+
+function category_load_widgets() {
+  register_widget( 'Category_Widget' );
+}
+
+add_action("plugins_loaded", "silexWidget_init");
+add_action( 'widgets_init', 'category_load_widgets' );
