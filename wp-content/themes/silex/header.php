77e212593337e8a6ed8e0eb29be7c70aff436d4e
Tom Gladhill <tomg@ubuntu.localdomain>
2011-05-04 08:51:01 -0700

    Fix layout issues associated with custom menus

--- wp-content/themes/silex/header.php
+++ wp-content/themes/silex/header.php
@@ -60,7 +60,7 @@
 					</a>
 				</div>
 					
-				<div id="access" role="navigation">
+				<div id="primary-nav" role="navigation">
 					<?php /* Our navigation menu.  If one isn't filled out,
 					wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used. 
 					 If none is assigned, the menu with the lowest ID is used.  */ ?>
